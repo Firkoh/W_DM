3aa1f7b2ca9479b344ace757a0995e9cfbf8904c
Firgenius Kolose Hombore <firgeniusigeni7799@gmail.com>
2024-08-23 01:25:38 +0900

    Penambahan User

---
+++ Web_User/home.php
@@ -0,0 +1,26 @@
+<?php
+include("session.php");
+
+// Include database connection
+include("../service/basisdata.php");
+
+?>
+
+
+    <?php include  "partials/header.html"?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>Selamat Datang di Website kantor Distrik Mawabuan, <?php echo $_SESSION['username']; ?></h1>
+                <p>
+                    <?php 
+                    // Insert your content here
+                    echo "Kantor Distrik Mawabuan berlokasi disamping Jln Trans Papua BaratDaya Kabupaten Tambrauw. Kantor Distrik Mawabuan salah satu distrik yang ada di Kabupaten Tambrauw dari 29 distrik. Pemerintah tambrauw membanggun kantor distrik mawabuan ini untuk mensejahterakanmasyarakat dalam pendidikan, ekonomi dan sosial. Adanya kantor distrikmawabuan meningkatkan kualitas pendidikan,merasakan kebutuhanuntuk mengembangkan sistem informasi dalam pendidikan maupunekonomi sosial.kantor distrik mawabuan membuat website gunamendukung operasi lantor distrik mawabuan yang lebih efisiendanefektif."
+                    ?>
+                </p>
+            </div>
+        </div>
+
+<!-- ini footer -->
+<?php include "partials/footer.html" ?>    
---
+++ Web_User/profila.php
@@ -0,0 +1,8 @@
+<?php include "session.php" ?>
+<?php 
+include "partials/header.html"
+ ?>
+     <div class="container mt-4">
+        <h3 class="text-center mb-4">Profil</h3>
+        <h4 class="text-left mb-4">Tentang Distrik Makbon</h4>
+<?php include "partials/footer.html" ?>
